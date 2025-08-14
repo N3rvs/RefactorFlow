510dd5eca7f869e41c5cf40adee5c046253ea743
Guillermo Longueira Lopez <nervelessdev@gmail.com>
2025-08-14 12:40:20 +0000

    esto esta largo: poco estetico.....por favor...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -626,7 +626,7 @@ export default function RefactorPage() {
             </header>
              <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 flex-1 min-h-0">
                 {/* Columna Izquierda */}
-                <div className="lg:col-span-2 h-full">
+                <div className="lg:col-span-2 h-full flex flex-col">
                     <SchemaEditor
                         schema={schema}
                         loading={loading === "schema"}
